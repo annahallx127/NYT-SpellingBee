13dfde7ecfb94c847f72f59dd4b0f717a70761ab
Anna Hall <hall.anna@northeastern.edu>
2024-03-12 00:04:12 -0700

    Milestone 1 complete

--- src/student/SpellingBee.java
+++ src/student/SpellingBee.java
@@ -2,6 +2,13 @@ package student;
 
 import edu.willamette.cs1.spellingbee.SpellingBeeGraphics;
 import java.awt.Color;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashSet;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class SpellingBee {
     private static final String ENGLISH_DICTIONARY = "res/EnglishWords.txt";
@@ -15,7 +22,11 @@ public class SpellingBee {
     }
 
     private void puzzleAction(String s) {
-        sbg.showMessage("puzzleAction is not yet implemented", Color.RED);
+        if (!validationHelperFunc(s)) {
+            validationHelperFunc(s);
+        } else {
+            sbg.setBeehiveLetters(s);
+        }
     }
 
     private boolean validationHelperFunc(String s) {
@@ -23,21 +34,49 @@ public class SpellingBee {
             sbg.showMessage("input is not 7 characters", Color.RED);
             return false;
         }
+
         for (int i = 0; i < s.length(); i++) {
             char element = s.charAt(i);
             if (!Character.isLetter(element)) {
-                sbg.showMessage("input is not part of the 26 letters");
+                sbg.showMessage("input is not part of the 26 letters", Color.RED);
                 return false;
             }
         }
+
+
+        HashSet<Character> newSet = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
-            
+            char characters = Character.toUpperCase(s.charAt(i));
+            if (newSet.add(characters) == false) {
+                sbg.showMessage("input has a letter that appears more than once", Color.RED);
+                return false;
+            }
         }
+        return true;
     }
 
+
+
     private void solveAction() {
+        sbg.clearWordList();
+        for (String word : dictionary();) {
+            if ()
+        }
         sbg.showMessage("solveAction is not yet implemented", Color.RED);
     }
+
+    private void isValidWord(String word){
+        centerLetter = toString().toUpperCase().charAt(0);
+        puzzleLetters = s.toLowerCase();
+        if (word.length() < 4 || !word.contains(Character.toString(centerLetter))) {
+            return false;
+            for (char e : word.toCharArray()){
+                if (!puzzleLetters;)
+            }
+
+        }
+
+
 
     public static void main(String[] args) {
         new SpellingBee().run();
