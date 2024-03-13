7077268cc5ad1824ccf4da8c43b842116dda1a03
Anna Hall <hall.anna@northeastern.edu>
2024-03-12 23:32:33 -0700

    Milestone 2 complete

--- src/student/SpellingBee.java
+++ src/student/SpellingBee.java
@@ -1,11 +1,13 @@
 package student;
 
 import edu.willamette.cs1.spellingbee.SpellingBeeGraphics;
+
 import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Locale;
 import java.util.Scanner;
@@ -15,18 +17,20 @@ public class SpellingBee {
 
     private SpellingBeeGraphics sbg;
     private char centerLetter;
+    private ArrayList<String> dictionary = new ArrayList();
+    private HashSet<Character> letterSet = new HashSet<>();
 
     public void run() {
         sbg = new SpellingBeeGraphics();
         sbg.addField("Puzzle", (s) -> puzzleAction(s));
-//        sbg.addButton("Solve", (s) -> solveAction());
+        sbg.addButton("Solve", (s) -> solveAction());
     }
 
     public void useDictionary() {
-        HashSet<String> dictionary = new HashSet<>();
         try {
             Scanner scanner = new Scanner(new File(ENGLISH_DICTIONARY));
             while (scanner.hasNextLine()) {
+
                 String word = scanner.nextLine().trim().toLowerCase();
                 dictionary.add(word);
             }
@@ -35,8 +39,6 @@ public class SpellingBee {
             throw new RuntimeException(e);
         }
     }
-
-
 
     private void puzzleAction(String s) {
         if (!validationHelperFunc(s)) {
@@ -61,12 +63,51 @@ public class SpellingBee {
             }
         }
 
-
-        HashSet<Character> newSet = new HashSet<>();
         for (int i = 0; i < s.length(); i++) {
             char characters = Character.toUpperCase(s.charAt(i));
-            if (newSet.add(characters) == false) {
+            if (letterSet.add(characters) == false) {
                 sbg.showMessage("input has a letter that appears more than once", Color.RED);
+                return false;
+            }
+
+        }
+        centerLetter = s.charAt(0);
+        return true;
+    }
+
+    private void solveAction() {
+        useDictionary();
+
+
+        for (int i = 0; i < dictionary.size(); i++) {
+            for (Character letter: dictionary.get(i).toCharArray()){
+                if (!letterSet.contains(letter)) {
+                        sbg.showMessage("does not contain any of the inputted letters", Color.RED);
+                }
+            }
+                if (isValidWord(dictionary.get(i))) {
+                    sbg.addWord(dictionary.get(i));
+                }
+            }
+
+        sbg.showMessage("");
+    }
+
+
+    private boolean isValidWord(String word) {
+        String puzzleLetters = word.toLowerCase();
+        if (word.length() < 4) {
+            sbg.showMessage("word is not four or more letters long", Color.RED);
+            return false;
+        }
+        if (!word.contains(Character.toString(centerLetter))) {
+            sbg.showMessage("word does not contain the center letter", Color.RED);
+            return false;
+        }
+        HashSet<Character> wordSet = new HashSet<>();
+        for (int i = 0; i < word.length(); i++) {
+            if (!letterSet.contains(Character.toUpperCase(puzzleLetters.charAt(i)))) {
+                sbg.showMessage("word does not contain one of given letters", Color.RED);
                 return false;
             }
         }
@@ -74,32 +115,11 @@ public class SpellingBee {
     }
 
 
-
-    private void solveAction() {
-        sbg.clearWordList();
-        for (String word : dictionary();) {
-            if ()
-        }
-        sbg.showMessage("solveAction is not yet implemented", Color.RED);
-    }
-
-    private void isValidWord(String word){
-        puzzleLetters = puzzleLetters.toLowerCase();
-        if (word.length() < 4 || !word.contains(Character.toString(centerLetter))) {
-            return false;
-            for (char e : word.toCharArray()) {
-                if (!puzzleLetters.contains(Character.toString(e))) {
-                    return false;
-                }
-                }
-            return true;
-
-            }
-
-
-
-
     public static void main(String[] args) {
         new SpellingBee().run();
     }
 }
+
+
+
+
